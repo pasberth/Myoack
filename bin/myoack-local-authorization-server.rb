9fcd9c0b4a643cd1ba70394ef429001fd99cb490
pasberth <pasberth@gmail.com>
2012-04-23 16:03:43 +0900

    fix README

--- bin/myoack-local-authorization-server.rb
+++ bin/myoack-local-authorization-server.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-$:.unshift File.dirname(__FILE__) + '/../lib'
 require 'myoack/as_lib'
 require 'myoack/local_authorization'
 Myoack::LocalAuthorization.run!(:host => Myoack::LocalAuthorization::HOST, :port => Myoack::LocalAuthorization::PORT)
